1a21f4b534f2b2a170c5ab6e6ee9b4fd9e1c8a2b
caroquintana <caro.quintana.n@gmail.com>
2017-06-03 01:09:58 -0400

    añadiendo el modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,38 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+
+(function modal(){
+  var traerGaleria = Array.from(document.getElementsByClassName("imgn"));
+  var modal = document.getElementById("cajamodal");
+
+  
+
+  var bodyModal, close, img;
+  
+
+  traerGaleria.forEach(function(box){
+    box.addEventListener("click", function(){
+      modal.innerHTML = "";
+      bodyModal = document.createElement("div");
+      bodyModal.classList.add("modal-body");
+      bodyModal.innerHTML = box.innerHTML;
+      modal.appendChild(bodyModal);
+      modal.classList.remove("hide");
+
+      close = document.createElement("div");
+      close.classList.add("close");
+
+      img = document.createElement("img");
+      img.setAttribute("src", "http://tojaeurope.com/gallery/Close-icon.png");
+
+      close.appendChild(img);
+      modal.appendChild(close);
+      close.addEventListener("click", function(){
+        modal.classList.add("hide");
+      });
+    });
+  });
+})();
+
+
